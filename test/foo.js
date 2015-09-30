65f9c062faa532d3c2d1f4bfd3140d88d45a57f8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-09-29 21:52:50 -0400

    example

--- test/foo.js
+++ test/foo.js
@@ -1,3 +1,4 @@
+// foo.js
 // function getFoo is not accessible from outside
 // how to unit test it?
 (function reallyPrivate() {
@@ -5,3 +6,11 @@
     return 'foo';
   }
 }());
+/*
+// foo-spec.js
+describeIt('./foo.js', 'getFoo()', function () {
+  it('returns "foo"', function () {
+    la(this.getFoo() === 'foo');
+  });
+});
+*/
